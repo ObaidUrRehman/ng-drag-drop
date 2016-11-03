af2586248fa8ca15285642d52692063aa235e738
ObaidUrRehman <obaid.urrehman@gmail.com>
2016-11-03 14:01:29 +0500

    Updated demo to show the dragHandle feature

--- demo/app/components/demo-component.ts
+++ demo/app/components/demo-component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'demo-app',
@@ -20,40 +20,45 @@ import {Component} from '@angular/core';
     .drag-target-border-green {
        border: #3c763d dashed 2px;
     }
+
+    .drag-handle {
+        cursor: move;
+    }
    `
     ]
 })
 export class DemoComponent {
 
-    items = [
-        {name: "Apple", type: "fruit"},
-        {name: "Carrot", type: "vegetable"},
-        {name: "Orange", type: "fruit"},
-        {name: "Mango", type: "fruit"},
-        {name: "Onion", type: "vegetable"},
-        {name: "Potato", type: "vegetable"},
-        {name: "Banana", type: "fruit"},
-        {name: "Guava", type: "fruit"},
-        {name: "Pear", type: "fruit"},
-        {name: "Capsicum", type: "vegetable"}];
+    vegetables = [
+        { name: "Carrot", type: "vegetable" },
+        { name: "Onion", type: "vegetable" },
+        { name: "Potato", type: "vegetable" },
+        { name: "Capsicum", type: "vegetable" }];
+
+    fruits = [
+        { name: "Apple", type: "fruit" },
+        { name: "Orange", type: "fruit" },
+        { name: "Mango", type: "fruit" },
+        { name: "Banana", type: "fruit" },
+        { name: "Pear", type: "fruit" }];
 
     droppedFruits = [];
     droppedVegetables = [];
 
     onFruitDrop(e: any) {
         this.droppedFruits.push(e.dragData);
-        this.removeItem(e.dragData);
+        this.removeItem(e.dragData, this.fruits);
     }
 
     onVegetableDrop(e: any) {
         this.droppedVegetables.push(e.dragData);
-        this.removeItem(e.dragData);
+        this.removeItem(e.dragData, this.vegetables);
     }
 
-    removeItem(item: any) {
-        let index = this.items.map((e)=> {
+    removeItem(item: any, list: Array<any>) {
+        let index = list.map((e) => {
             return e.name
-        }) .indexOf(item.name);
-        this.items.splice(index, 1);
+        }).indexOf(item.name);
+        list.splice(index, 1);
     }
 }
