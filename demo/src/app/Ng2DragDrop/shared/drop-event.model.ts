8183ae21613b0305ecfb52b1026b5ad0e6d5b8b1
ObaidUrRehman <obaid.urrehman@gmail.com>
2017-04-07 15:48:50 +0500

    chore(): removed Ng2DragDrop folder from demo app

--- demo/src/app/Ng2DragDrop/shared/drop-event.model.ts
+++
@@ -1,9 +0,0 @@
-export class DropEvent {
-    nativeEvent: any;
-    dragData: any;
-
-    constructor(event: any, data: any) {
-        this.nativeEvent = event;
-        this.dragData = data;
-    }
-}
